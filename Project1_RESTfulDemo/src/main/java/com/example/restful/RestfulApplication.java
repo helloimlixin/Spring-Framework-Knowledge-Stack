e2546761a9a505537d041a9d97e263a2ab07a7e9
Xin Li <bulixin@bu.edu>
2020-02-15 08:41:50 +0800

    Created docs folder

---
+++ Project1_RESTfulDemo/src/main/java/com/example/restful/RestfulApplication.java
@@ -0,0 +1,19 @@
+package com.example.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class RestfulApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestfulApplication.class);
+    }
+}
